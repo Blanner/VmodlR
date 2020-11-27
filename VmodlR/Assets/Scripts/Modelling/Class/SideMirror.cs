5e1469e6bb2ea5dbdf6224a7f2446383f630d6e6
Simon Gorissen <scg@mail.upb.de>
2020-11-27 17:13:05 +0100

    Switch class name synchronization to events.
    This improves the networking overhead tremendously

--- VmodlR/Assets/Scripts/Modelling/Class/SideMirror.cs
+++ VmodlR/Assets/Scripts/Modelling/Class/SideMirror.cs
@@ -3,19 +3,58 @@ using System.Collections.Generic;
 using UnityEngine;
 
 using Photon.Pun;
+using Photon.Realtime;
+using ExitGames.Client.Photon;
 
-public class SideMirror : MonoBehaviourPun, IPunObservable
+using Hashtable = ExitGames.Client.Photon.Hashtable;
+
+public class SideMirror : MonoBehaviourPun, IOnEventCallback
 {
+    public const byte synchronizeClassEventCode = 1;
+
     public List<ClassSide> sides;
 
-    public string className = "ClassName";
-    public bool syncLocalChange = false;
+    public string className = "Class - Name";
+
+    void Start()
+    {
+        localChangeName(className);
+    }
+
+    private void OnEnable()
+    {
+        PhotonNetwork.AddCallbackTarget(this);
+    }
+
+    private void OnDisable()
+    {
+        PhotonNetwork.RemoveCallbackTarget(this);
+    }
 
     public void OnChangedName(string newName)
     {
         photonView.RequestOwnership();
-        localChangeName(newName);
-        syncLocalChange = true;
+
+        //recreate the content of the last change event received
+        Hashtable oldContent = new Hashtable();
+        oldContent.Add("PhotonViewID", photonView.ViewID);
+        oldContent.Add("NewClassName", className);//this is still the "old" class name
+
+        //delete the last change event from the room's event cache by filtering by its content
+        RaiseEventOptions deleteEventOptions = new RaiseEventOptions { Receivers = ReceiverGroup.All, CachingOption = EventCaching.RemoveFromRoomCache };
+        if(!PhotonNetwork.RaiseEvent(synchronizeClassEventCode, oldContent, deleteEventOptions, SendOptions.SendReliable))
+        {
+            Debug.LogError("Event was not deleted!");
+        }
+
+        //recreate the content of the last change event received
+        Hashtable newConent = new Hashtable();
+        newConent.Add("PhotonViewID", photonView.ViewID);
+        newConent.Add("NewClassName", newName);
+
+        //raise the new change event, that replaces the one we just deleted
+        RaiseEventOptions createEventOptions = new RaiseEventOptions { Receivers = ReceiverGroup.All, CachingOption = EventCaching.AddToRoomCacheGlobal };
+        PhotonNetwork.RaiseEvent(synchronizeClassEventCode, newConent, createEventOptions, SendOptions.SendReliable);
     }
 
     private void localChangeName(string newName)
@@ -27,21 +66,20 @@ public class SideMirror : MonoBehaviourPun, IPunObservable
         }
     }
 
-    public void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info)
+    public void OnEvent(EventData photonEvent)
     {
-        //Only write if we requested and got ownership (we request when OnChangedName is called)
-        if(stream.IsWriting)
+        //Check if the event is a synchronization event for a class
+        if(photonEvent.Code == synchronizeClassEventCode)
         {
-            stream.SendNext(className);
-        }
-        else
-        {
-            object receivedMessage = stream.ReceiveNext();
-            if ( receivedMessage != null)
+            //extract the sent data from the event
+            Hashtable eventData = (Hashtable)photonEvent.CustomData;
+            int synchronizeClassViewID = (int)eventData["PhotonViewID"];
+            //check if the class that the event wants to synchronize is this class
+            if(synchronizeClassViewID == photonView.ViewID)
             {
-                localChangeName((string)receivedMessage);
+                string newClassName = (string)eventData["NewClassName"];
+                localChangeName(newClassName);
             }
         }
     }
-
 }
