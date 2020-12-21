4e69b77d505e4daa0c806ad208e02e0d710dae90
Simon Gorissen <scg@mail.upb.de>
2020-12-21 10:19:21 +0100

    Refined Player Avatar
    - Added PlayerName Plates over Avatars
    - Separated Body and Head of Player Avatar

---
+++ VmodlR/Assets/Scripts/PlayerMenu/PlayerNameText.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+using Photon.Pun;
+
+[RequireComponent(typeof(Text))]
+public class PlayerNameText : MonoBehaviour
+{
+    public PhotonView playerPhotonView;
+
+    private Text nameText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        nameText = GetComponent<Text>();
+        nameText.text = playerPhotonView.Owner.NickName;
+        enabled = false;
+    }
+}
